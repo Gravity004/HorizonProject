e6c41cabce4d7cc3f9354ad3791882606fa2930c
nipapohn0001 <rinradauu@gmail.com>
2026-03-06 22:51:02 +0700

    fix syntax

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ app.get(/^\/dashboard/, checkGuildMembership, (req, res) => {
 // Serve Vue SPA (only in production)
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'frontend/dist')));
-    app.get('*', (req, res) => {
+    app.get('/{*path}', (req, res) => {
         res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
     });
 }
